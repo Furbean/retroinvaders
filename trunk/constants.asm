d796d60b70682aa7c521a20dd62a5aed7b513a6e
peter.welzien@gmail.com <peter.welzien@gmail.com@506bd0aa-219b-04d9-13d9-c165bb4a5903>
2012-12-07 17:10:21 +0000

    Wouldn't compile after I've removed bug.asm

--- trunk/constants.asm
+++ trunk/constants.asm
@@ -3,35 +3,30 @@
         *=$08ff
 
 ; zero page constants
-$io_port        = $00
-$io_bank        = $01
+io_port$        = $00
+io_bank$        = $01
 
-
-$src_ptr        = $02
-$src_ptr_lo     = $02
-$src_ptr_hi     = $03
-$dst_ptr        = $04
-$src_ptr_lo     = $04
-$src_ptr_hi     = $05
+src_ptr$        = $02
+dst_ptr$        = $04
 
 
 ; contains high byte of the bank address
-$vic_bank       = $f0
-$screen_bank    = $f1
-$char_bank      = $f2
-$bitmap_bank    = $f3
+vic_bank$       = $f0
+screen_bank$    = $f1
+char_bank$      = $f2
+bitmap_bank$    = $f3
 
 
 ; IO addresses
-$vic_io         = $d000
-$sid_io         = $d400
-$irq_io         = $dc00
-$nmi_io         = $dd00
+vic_io$         = $d000         ; VIC-II io address
+sid_io$         = $d400         ; SID address
+irq_io$         = $dc00         ; CIA #1 IRQ io address
+nmi_io$         = $dd00         ; CIA #2 NMI io address
 
 
-$color_ram      = $d800        ; color ram address
+color_ram$      = $d800        ; color ram address
 
 
-$rst_addr       = $fffc         ; reset address
-$nmi_addr       = $fffa         ; nmi address
-$irq_addr       = $fffe         ; irq address
+rst_addr$       = $fffc         ; reset address
+nmi_addr$       = $fffa         ; nmi address
+irq_addr$       = $fffe         ; irq address
