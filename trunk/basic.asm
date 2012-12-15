b5264eb26ab22d73d3526e09593b98c2d69877be
peter.welzien@gmail.com <peter.welzien@gmail.com@506bd0aa-219b-04d9-13d9-c165bb4a5903>
2012-12-15 19:01:08 +0000

    Small project updates

--- trunk/basic.asm
+++
@@ -1,9 +0,0 @@
-; retroinvaders
-
-; contains basic startup code
-
-        *=$801
-
-        ; 10 SYS (2304): REM $900
-        BYTE        $0E, $08, $0A, $00, $9E, $20, $28,  $32, $33, $30, $34, $29, $00, $00, $00
-
